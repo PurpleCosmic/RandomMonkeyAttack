5068d6650278738cadef060ff2135b69d676a3e2
elerhels <elias.verhelst@ugent.be>
2025-05-03 20:43:30 +0200

    monkeys and fries

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import os
 import sys
 import signal
 import random
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QShortcut
 from PyQt5.QtGui import QKeySequence, QMovie
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 
 class Window(QMainWindow):
     def __init__(self):
@@ -23,11 +24,30 @@ class Window(QMainWindow):
         self.setGeometry(QApplication.primaryScreen().geometry())
         
         self.label = QLabel(self)
+        self.label.setScaledContents(True)
         self.label.setGeometry(self.rect())
 
-        movie = QMovie("videos/monkey.gif")
-        self.label.setMovie(movie)
-        movie.start()
+        self.setup_gif(self.get_next_gif())
+
+    def setup_gif(self, gif_path):
+        self.movie = QMovie(gif_path)
+        self.label.setMovie(self.movie)
+        self.movie.frameChanged.connect(self.check_loop_end)
+        QTimer.singleShot(random.randint(1000 * 60, 1000 * 60 * 60 * 8), self.play_gif)
+
+    def play_gif(self):
+        self.label.show()
+        self.movie.start()
+
+    def get_next_gif(self):
+        gif_files = [f for f in os.listdir("videos") if f.lower().endswith('.gif')]
+        return os.path.join("videos", random.choice(gif_files))
+
+    def check_loop_end(self, frame_number):
+        if frame_number == self.movie.frameCount() - 1:
+            self.movie.stop()
+            self.label.hide()
+            self.setup_gif(self.get_next_gif())
 
 
 if __name__ == '__main__':
