9a34842371b953373d4d56d62c1d9d4bb27a8c2c
elerhels <elias.verhelst@ugent.be>
2025-05-04 18:14:47 +0200

    the monkeys and the apes retaliate by means of debate

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QShortcut
 from PyQt5.QtGui import QKeySequence, QMovie
 from PyQt5.QtCore import Qt, QTimer
 
+def get_base_path():
+    if getattr(sys, 'frozen', False):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.dirname(__file__)
+    return base_path
+
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -34,15 +41,19 @@ class Window(QMainWindow):
         self.movie = QMovie(gif_path)
         self.label.setMovie(self.movie)
         self.movie.frameChanged.connect(self.check_loop_end)
-        QTimer.singleShot(random.randint(1000 * 60, 1000 * 60 * 60 * 8), self.play_gif)
+        QTimer.singleShot(
+            random.randint(1000 * 60, 1000 * 60 * 60 * 1), 
+            self.play_gif
+        )
 
     def play_gif(self):
         self.label.show()
         self.movie.start()
 
     def get_next_gif(self):
-        gif_files = [f for f in os.listdir("videos") if f.lower().endswith('.gif')]
-        return os.path.join("videos", random.choice(gif_files))
+        videos_path = os.path.join(get_base_path(), "videos")
+        gif_files = [f for f in os.listdir(videos_path) if f.lower().endswith('.gif')]
+        return os.path.join(videos_path, random.choice(gif_files))
 
     def check_loop_end(self, frame_number):
         if frame_number == self.movie.frameCount() - 1:
